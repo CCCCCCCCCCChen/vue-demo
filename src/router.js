422664a5e22d008815a87e9b69007230640d851c
JY614878398 <614878398@qq.com>
2019-04-11 15:51:57 +0800

    iview 的 Layout 布局

---
+++ src/router.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from './views/Home.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'iviewPage',
+      component: () => import(/* webpackChunkName: "iviewPage" */ './components/page/iviewPage.vue')
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    }
+  ]
+})
